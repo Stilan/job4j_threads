60888a02275d183154a53ed9eeb733253af94548
Stilan <40999153+Stilan@users.noreply.github.com>
2021-10-26 21:51:55 +0700

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/coccurent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.coccurent;
+
+public class Wget {
+    public static void main(String[] args) {
+            Thread thread = new Thread(
+                    () -> {
+                        try {
+                            for (int i = 0; i <= 100; i++) {
+                                System.out.print("\rLoading : " + i + "%");
+                                Thread.sleep(1000);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            );
+            thread.start();
+    }
+}
