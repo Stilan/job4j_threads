0795311f6b953d1e4f02d6f58ae294decea7dbcb
Stilan <40999153+Stilan@users.noreply.github.com>
2021-10-27 12:54:30 +0700

    1.1. Состояние нити. [#229175 #220816]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -2,18 +2,22 @@ package ru.job4j.concurrent;
 
 public class ThreadState {
     public static void main(String[] args) {
-        Thread first = null;
-        for (int i = 0; i < 2; i++) {
-            first = new Thread(
+        Thread  first = new Thread(
                     () -> System.out.println(Thread.currentThread().getName())
             );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
             System.out.println(first.getState());
             first.start();
-            while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getState());
+            second.start();
+            while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
                 System.out.println(first.getState());
+                System.out.println(second.getState());
             }
             System.out.println(first.getState());
-        }
+            System.out.println(second.getState());
             System.out.println("Работа завершена");
     }
 }
