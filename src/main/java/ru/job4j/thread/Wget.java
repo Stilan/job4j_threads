25c7dfa9acce0190c93c465907b412dd3c881315
Stilan <40999153+Stilan@users.noreply.github.com>
2021-10-29 16:15:15 +0700

    4. Скачивание файла с ограничением. [#144271 #221678]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -3,6 +3,13 @@ package ru.job4j.thread;
 import java.io.*;
 import java.net.URL;
 
+/**
+ * В каких единицах у вас измеряется speed?
+ *  Миллисекунда равна 1/1000 секунды
+ *  speed = 1 мБит/с = 1/1000 мин секунд = 0,001
+ *  System.currentTimeMillis() = мин секунд
+ */
+
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
@@ -24,9 +31,12 @@ public class Wget implements Runnable {
                    fileOutputStream.write(dataBuffer, 0, bytesRead);
                    long end = System.currentTimeMillis();
                    long time = end - start;
-                   if (time < speed) {
-                       Thread.sleep(1000);
+                   System.out.println(time);
+                   var speedTimeBit = speed / 1000;
+                   if (time < speedTimeBit) {
+                       Thread.sleep((speedTimeBit - time) * 1000);
                    }
+
                }
            } catch (IOException e) {
                e.printStackTrace();
@@ -35,12 +45,24 @@ public class Wget implements Runnable {
             Thread.currentThread().interrupt();
         }
     }
+    private static boolean validatorArgs(String[] args) {
+       if (args.length != 2) {
+           return false;
+       }
+        return  true;
+    }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
-        wget.start();
-        wget.join();
+        if (validatorArgs(args)) {
+            String url = args[0];
+            int speed = Integer.parseInt(args[1]);
+            Thread wget = new Thread(new Wget(url, speed));
+            wget.start();
+            wget.join();
+        } else {
+            throw new IllegalArgumentException("Parameters entered incorrectly ");
+        }
     }
+
+
 }
