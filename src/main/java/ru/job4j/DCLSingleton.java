06e02d4294c05d155c7f0fa329d66ea19bcdecbe
Stilan <40999153+Stilan@users.noreply.github.com>
2021-10-30 13:07:24 +0700

    2. Модель памяти Java [#267917]

--- src/main/java/ru/job4j/DCLSingleton.java
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -1,17 +1,5 @@
 package ru.job4j;
 
-/**
- *  без volatile
- *  Проблема идиомы Double Checked Lock заключается в модели памяти Java,
- *  точнее в порядке создания объектов. Можно условно представить этот порядок следующими этапами [2, 3]:
- * Пусть мы создаем нового студента: DCLSingleton s = new DCLSingleton(), тогда
- * 1) local_ptr = DCLSingleton()) // выделение памяти под сам объект;
- * 2) s = local_ptr // инициализация указателя;
- * 3) DCLSingleton(s); // конструирование объекта (инициализация полей);
- * Таким образом, между вторым и третьим этапом возможна ситуация,
- * при которой другой поток может получить и начать использовать
- * (на основании условия, что указатель не нулевой) не полностью сконструированный объект.
- */
 public class DCLSingleton {
     private static volatile DCLSingleton inst;
 
@@ -25,12 +13,6 @@ public class DCLSingleton {
         }
         return inst;
     }
-
     private DCLSingleton() {
-    }
-
-    public static void main(String[] args) {
-        DCLSingleton singleton = new DCLSingleton();
-        System.out.println(instOf());
     }
 }
