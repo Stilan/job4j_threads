85d8c56290a477799ae63fe0e10772429d8aac8c
Stilan <40999153+Stilan@users.noreply.github.com>
2021-11-08 17:34:56 +0700

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,52 @@
+package ru.job4j.buffer;
+
+import ru.job4j.producer.SimpleBlockingQueue;
+
+/**
+ * «Ядовитая таблетка» - это общее решение для остановки или прерывания потоков как производителя, так и потребителя.
+ * Идея состоит в том, что производитель помещает «ядовитую таблетку» в очередь и выходит,
+ * если «потребитель» видит «ядовитую таблетку», то останавливается и выходит.
+ * @param <T>
+ */
+public class ParallelSearch<T> {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
+        final Integer POISON = 900;
+
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        try {
+                            Integer i = queue.poll();
+                            if (i == POISON) {
+                                break;
+                            }
+                            System.out.println(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    try {
+                        queue.offer(POISON);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+        ).start();
+    }
+}
