17cb3696dfdff66f94ad9a0ca3b36120597c020b
Stilan <40999153+Stilan@users.noreply.github.com>
2021-11-28 13:51:43 +0700

    3.1. Добавить изменения в последний коммит.[#306989]

--- src/main/java/ru/job4j/pools/MergeSort.java
+++ src/main/java/ru/job4j/pools/MergeSort.java
@@ -22,10 +22,10 @@ public class MergeSort {
         int ri = 0;
         int resI = 0;
         int[] result = new int[left.length + right.length];
-        while (resI !=   result.length) {
+        while (resI != result.length) {
             if (li == left.length) {
                 result[resI++] = right[ri++];
-            } else if (ri == right.length) {
+            } else if (ri ==  right.length) {
                 result[resI++] = left[li++];
             } else if (left[li] <= right[ri]) {
                 result[resI++] = left[li++];
