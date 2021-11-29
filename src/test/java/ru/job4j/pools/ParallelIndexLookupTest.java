2a5cd73f8da5b7d86d0292de9eef0ea2024ca4bf
Stilan <40999153+Stilan@users.noreply.github.com>
2021-11-29 17:25:31 +0700

    3.3. Объединение нескольких коммитов. [#33084]V3

--- src/test/java/ru/job4j/pools/ParallelIndexLookupTest.java
+++ src/test/java/ru/job4j/pools/ParallelIndexLookupTest.java
@@ -10,7 +10,8 @@ public class ParallelIndexLookupTest  {
     @Test
     public void indexSearch() {
         int[] array = new int[]{1, 2, 4, 6, 10, 23,
-                45, 18, 9, 7, 77, 66, 41, 51, 35, 90, 23, 55, 11, 13};
+                45, 18, 9, 7, 77, 66, 41,
+                51, 35, 90, 23, 55, 11, 13};
         ParallelIndexLookup parallelIndexLookup = new ParallelIndexLookup(array, 0, array.length, 11);
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
         int index = forkJoinPool.invoke(parallelIndexLookup);
@@ -19,7 +20,8 @@ public class ParallelIndexLookupTest  {
     @Test
     public void notFoundIndexSearch() {
         int[] array = new int[]{1, 2, 4, 6, 10, 23,
-                45, 18, 9, 7, 77, 66, 41, 51, 35, 90, 23, 55, 11, 13};
+                45, 18, 9, 7, 77, 66, 41,
+                51, 35, 90, 23, 55, 11, 13};
         ParallelIndexLookup parallelIndexLookup = new ParallelIndexLookup(array, 0, array.length, 0);
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
         int index = forkJoinPool.invoke(parallelIndexLookup);
